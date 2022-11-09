d8b5557d4f088100609447e8114a2f230a50f303
Sofia <315209@via.dk>
2022-11-09 14:24:46 +0100

    blazorwasm commit

---
+++ BlazorWASM/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorWASM;
+using HttpClients;
+using HttpClients.ClientImplementations;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7056") });
+builder.Services.AddScoped<IUserService, UserHttpClient>();
+await builder.Build().RunAsync();
